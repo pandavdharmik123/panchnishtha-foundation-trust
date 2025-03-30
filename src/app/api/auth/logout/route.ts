8455692d2f1e056fb09fcea95f7d9269fd9a4f7d
Dharmik Pandav <pandavdharmik123@gmail.com>
2025-03-30 17:44:52 +0530

    fix: logout issue

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 
-export async function GET() {
-  const response = NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_BASE_URL));
+export async function GET(request: NextRequest) {
+  const response = NextResponse.redirect(new URL("/login", request.nextUrl.origin));
 
   // ✅ Remove token by setting an expired cookie
   response.cookies.set("token", "", {
